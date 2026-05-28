1d829f2d7d37819aa6fae22de6a542cc718a4e78
Anuj-Bisht-dev <anujbisht.dev@gmail.com>
2026-05-28 22:15:49 +0530

    configure techstack and node server

--- express/Auth-typescript-drizzle-postgres/src/index.ts
+++ express/Auth-typescript-drizzle-postgres/src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello world");
+import {createServer} from "node:http";
+
+async function main() {
+    try{ 
+        const server = createServer()
+        const PORT:number = 5000;
+
+        server.listen(PORT, () => {
+            console.log(`http server is runnning on ${PORT} port`);
+        })
+    }catch(error){
+        console.log("Error strarted in http server");
+        throw error;
+    }   
+}
+
+main();
